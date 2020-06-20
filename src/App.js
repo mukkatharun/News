95bffcbcf21d22828885b987921f04407828abe3
Tharun Mukka <tharunmukka1@gmail.com>
2020-06-21 01:17:01 +0530

    added search feature

--- src/App.js
+++ src/App.js
@@ -1,23 +1,88 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 import './App.css';
+import list from './list';
+import { compare } from 'semver';
+// import Loading from './Loading';
+
+const isSearched = (sTerm) => {
+  return (item) => !sTerm || item.title.toLowerCase().includes(sTerm.toLowerCase());
+}
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state ={
-      user:[]
+      list,
+      searchTerm: ''
     }
+    this.removeItem = this.removeItem.bind(this);
+    this.searchValue = this.searchValue.bind(this);
   }
 
-  componentWillMount(){
-    axios('https://api.randomuser.me/?nat=US&results=5')
-    .then(response => this.setState({user: response.data.results}))
+  removeItem(id) {
+   const updatedList = this.state.list.filter(item => item.objectID !== id);
+   this.setState({list: updatedList});
+  }
+
+  searchValue(e) {
+    this.setState({searchTerm: e.target.value});
   }
 
   render() {
-    return <div>hi check</div>
+    const{list, searchTerm} = this.state;
+    return (
+      <div className="App">
+        <Search
+          onChange={this.searchValue} 
+          value={searchTerm}
+        />
+        <Table 
+          list={list}
+          searchTerm={searchTerm}
+          removeItem={this.removeItem}
+        />
+          
+      </div>
+    );
+  }
+}
+
+class Search extends Component {
+  render() {
+    const {onChange, value} = this.props;
+    return (
+      <form>
+          <input 
+          type="text" 
+          onChange={onChange} 
+          value={value}/>
+        </form>
+    );
+  }
+}
+
+class Table extends Component {
+  render() {
+    const {list, searchTerm, removeItem} = this.props;
+    return (
+      <div>
+        {
+            list.filter(isSearched(searchTerm)).map(item => 
+             {
+               return (
+                 <div key={item.objectID}>
+                    <h2><a href={item.url}> {item.title} </a> by {item.author}</h2>
+                    <h4>{item.num_comments} comments | {item.points} points</h4>
+                    <button type="button" onClick={() => removeItem(item.objectID)} value="remove">remove</button>
+                 </div>
+               );
+             }
+             )
+          }
+      </div>
+    );
   }
 }
 export default App;
---
+++ src/list.js
@@ -0,0 +1,28 @@
+const list = [
+    {
+      title: 'Tell me the truth without words',
+      url: 'https://www.facebook.com',
+      author: 'Ryan',
+      num_comments: 100,
+      points: 50,
+      objectID: 1
+    },
+    {
+      title: 'Oh no! The candle is out',
+      url: 'https://www.facebook.com',
+      author: 'Zen',
+      num_comments: 10,
+      points: 5,
+      objectID: 2
+    },
+    {
+      title: 'The hard earned black belt',
+      url: 'https://www.facebook.com',
+      author: 'Peter',
+      num_comments: 20,
+      points: 12,
+      objectID: 3
+    }
+  ]
+
+  export default list;
